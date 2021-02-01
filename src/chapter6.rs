e37331c5b9f44c0fff36081374348624052ac22c
Nordine Bittich <nordine.bittich@ext.europa.ec.eu>
2021-02-01 08:43:14 +0100

    chapter 6

--- src/chapter6.rs
+++ src/chapter6.rs
@@ -3,14 +3,25 @@ use std::fmt::Display;
 pub fn run() {
     let status1 = Status::SUCCESS(200, "ohlala".to_string());
     let status2 = Status::FORBIDDEN(403);
+    let status3 = Status::ERROR(500, "server".to_string());
+
     run_status(&status1);
     run_status(&status2);
 
-    run_option(Some(50));
+    run_option(Some("50"));
     run_option(Some("j'ai la dalle"));
+    run_option(None);
+
+    let (status_code,message) = status3.extract();
+
+    if let Some(msg) = message {
+        println!("{}: {}", status_code, msg);
+    } else {
+        println!("{}", status_code);
+    }
 }
 
-fn run_option(arg: Option<impl Display>) {
+fn run_option(arg: Option<&str>) {
     if let Some(val)  = arg {
         println!("{}", val);
     }
@@ -30,4 +41,16 @@ enum Status {
     UNAUTHORIZED(u32),
     ACCEPTED(u32),
     ERROR(u32, String)
+}
+
+impl Status {
+    fn extract (&self) -> (&u32, Option<&String>) {
+        match self {
+            Status::SUCCESS(status_code, message) => (status_code,Some(message)),
+            Status::ERROR(status_code, message) => (status_code,Some(message)),
+            Status::FORBIDDEN(status_code) => (status_code, None),
+            Status::UNAUTHORIZED(status_code) => (status_code,None),
+            Status::ACCEPTED(status_code) => (status_code,None),
+        }
+    }
 }
