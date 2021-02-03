e4a581e23f4fff8c5d0af75ec5a005b8af935bbf
Nordine Bittich <nordine@bittich.be>
2021-02-03 20:42:15 +0100

    chapter 7 part 1

--- src/lib.rs
+++ src/lib.rs
@@ -29,20 +29,70 @@ pub mod account {
             }
         }
     }
-    pub mod service {
-        use rand::{distributions::Alphanumeric,Rng};
 
-        use super::entity::{Role};
 
-        pub fn random_password () -> String {
-            rand::thread_rng().sample_iter(&Alphanumeric).take(8).map(char::from).collect()
-        }
+        pub mod store {
+            use super::entity::{User, Role};
 
-        pub fn random_role () -> Role {
-            match rand::thread_rng().gen_bool(0.5) {
-                true => Role::ADMIN,
-                false => Role::USER
+            pub enum NoSqlEntry {
+                USER(User),
+                SESSION(String)
             }
+
+            pub struct Storage(pub Vec<NoSqlEntry>);
+
+            impl Storage {
+                fn push(&mut self, entry: NoSqlEntry) {
+                    self.0.push(entry);
+                }
+
+            }
+
+            pub mod service {
+                use rand::{distributions::Alphanumeric,Rng};
+
+                use crate::account::store::{Role, NoSqlEntry, Storage};
+                use crate::account::entity::User;
+
+                pub fn random_password () -> String {
+                    rand::thread_rng().sample_iter(&Alphanumeric).take(8).map(char::from).collect()
+                }
+
+                pub fn random_role () -> Role {
+                    match rand::thread_rng().gen_bool(0.5) {
+                        true => Role::ADMIN,
+                        false => Role::USER
+                    }
+                }
+
+                pub fn add_user_to_store(storage: &mut Storage, user: User)  {
+                    storage.push(NoSqlEntry::USER(user));
+                }
+
+                pub fn add_session_to_store(storage:&mut Storage, session: String) {
+                    storage.push(NoSqlEntry::SESSION(session));
+                }
+
+                pub fn find_user_by_lastname(storage:Storage, lastname: &str) -> Option<User> {
+                    let mut resp: Option<User> = None;
+                    for item in storage.0 {
+                        match item {
+                            NoSqlEntry::USER(user) => {
+                                if user.lastname.eq(lastname) {
+                                    resp = Some(user);
+                                    break;
+                                }
+                            },
+                            _ => ()
+                        }
+                    }
+                    resp
+                }
+
+            }
+
+
         }
-    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use testrust::account::entity::{User, Role};
-use testrust::account::service::random_password;
-use testrust::account::service::random_role;
+use testrust::account::store::service::{random_password, add_session_to_store, find_user_by_lastname};
+use testrust::account::store::service::random_role;
+use testrust::account::store::service::add_user_to_store;
+use testrust::account::store::Storage;
 
 fn main() {
     let user: User = User::create_user("Nordine".to_string(),"Bittich".to_string(), 32, Role::ADMIN, "strong_password".to_string());
@@ -8,4 +10,12 @@ fn main() {
     println!("Password matches: {}", user.check_password("strong_password"));
     println!("Password random: {}", random_password());
     println!("Role random: {:?}", random_role());
+
+    let mut storage: Storage = Storage(Vec::new());
+    add_user_to_store(&mut storage, user);
+    add_session_to_store(&mut storage, "ouloulou".to_string());
+    let user_found = find_user_by_lastname(storage, "Bittich");
+    if let Some(usr) = user_found {
+        println!("it's a mess but it works => {:?}", usr);
+    }
 }
