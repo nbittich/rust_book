9843581c349b5a5b58356aec8363df3297257ef2
Nordine Bittich <nordine@bittich.be>
2021-02-05 07:39:43 +0100

    chapter 8

--- src/bin/chapter8.rs
+++ src/bin/chapter8.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::ops::{Div};
+
 fn main() {
 
     let  mut vector = vec![1,22,33,44];
@@ -19,5 +22,32 @@ fn main() {
         println!("{}", c);
 
     }
+    let (median,avg,map) = exercise1(&mut vec![4,3,2,1,4,4,7,2,3,9,1,7,8]);
+    println!("median: {}, avg: {}", median, avg);
+    map.iter().for_each(|e|{
+        println!("occurrence for {}: {}",e.0, e.1);
+    })
 
+}
+
+fn exercise1(list_of_integers:&mut Vec<u32>) -> (u32, u32, HashMap<u32, u32>) {
+    let len:usize = list_of_integers.len() - 1;
+    let mut avg = 0;
+    let mut occurrences: HashMap<u32, u32> = HashMap::new();
+
+    list_of_integers.sort();
+    let median =  list_of_integers[len / 2];
+
+
+    list_of_integers.iter().for_each(|i| {
+        let x = *i;
+        let count = occurrences.entry(x).or_insert(0);
+        *count+=1;
+        avg += x;
+    });
+
+    let avg:u32 = avg.div(len as u32);
+
+
+   (median, avg, occurrences)
 }
